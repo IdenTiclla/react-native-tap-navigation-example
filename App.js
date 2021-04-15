22be9754fcf8acc9e9df6f3987564f081de571d8
unknown <iden.ticlla@gmail.com>
2021-04-15 06:17:08 -0400

    navigation implemented

--- App.js
+++ App.js
@@ -2,20 +2,115 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Icon from 'react-native-vector-icons/Ionicons'
+
+import {createAppContainer} from 'react-navigation'
+import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs'
+
+
+
+
+class HomeScreen extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Home Screeen</Text>
+            </View>
+        )
+    }
+}
+
+class ProfileScreen extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Profile Screeen</Text>
+            </View>
+        )
+    }
+}
+
+class HistoryScreen extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>History Screeen</Text>
+            </View>
+        )
+    }
+}
+
+class CartScreen extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Cart Screeen</Text>
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
+
+
+const TabNavigator = createMaterialBottomTabNavigator(
+    {
+        Home: {
+            screen: HomeScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor}) => (
+                    <View>
+                        <Icon style={[{color: tintColor}]} size={25} name={'ios-home'}></Icon>
+                    </View>
+                )
+            }
+        },
+        Profile: {
+            screen: ProfileScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor}) => (
+                    <View>
+                        <Icon style={[{color: tintColor}]} size={25} name={'ios-person'}></Icon>
+                    </View>
+                )
+            }
+        },
+        History: {
+            screen: HistoryScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor}) => (
+                    <View>
+                        <Icon style={[{color: tintColor}]} size={25} name={'ios-images'}></Icon>
+                    </View>
+                )
+            }
+        },
+        Cart: {
+            screen: CartScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor}) => (
+                    <View>
+                        <Icon style={[{color: tintColor}]} size={25} name={'compass-outline'}></Icon>
+                    </View>
+                )
+            }
+        },
+    },
+
+    {
+        initialRouteName: 'Home',
+        activeColor: '#f0edf6',
+        inactiveColor: '#3e2465',
+        barStyle: { backgroundColor: '#694fad' },
+    }
+)
+
+// para cambiar iconos ve a la pagina ionicons 
+
+export default createAppContainer(TabNavigator)
