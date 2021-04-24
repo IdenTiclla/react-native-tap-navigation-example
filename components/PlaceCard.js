d87c15a3abe6c97a9df60b2af26cc8f3c607cfb1
unknown <iden.ticlla@gmail.com>
2021-04-24 09:10:25 -0400

    add PlaceCard Component

---
+++ components/PlaceCard.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper'; //yarn add react-native-paper
+
+
+import { View, Alert } from 'react-native'
+
+
+
+export default class PlaceCard extends React.Component {
+    render() {
+        return (
+            <View>
+                <Card onPress={()=> console.log('tocaste el card')}>
+                    <Card.Content>
+                        <Title>Card title</Title>
+                        <Paragraph>Card content</Paragraph>
+                    </Card.Content>
+                    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+                    <Card.Actions>
+                        <Button onPress={()=>console.log('hiciste click en el objeto cancel')}>Cancel</Button>
+                        <Button onPress={()=>console.log('hiciste click en el ok')}>Ok</Button>
+                    </Card.Actions>
+                </Card>
+            </View>
+        )
+    }
+}
